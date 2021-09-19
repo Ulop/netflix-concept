2a33341f0de622ef625a1c3142164d94d851a3f2
UlopLT <uloplt@gmail.com>
2021-09-19 20:42:55 +0300

    IconButton function

---
+++ src/main/kotlin/controls/IconButton.kt
@@ -0,0 +1,27 @@
+package controls
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+import base.Colors
+
+@Composable
+fun IconButton(imageVector: ImageVector, onClick: () -> Unit) {
+    Image(
+        imageVector,
+        null,
+        colorFilter = ColorFilter.tint(Colors.Primary),
+        modifier = Modifier
+            .size(24.dp)
+            .clip(CircleShape)
+            .clickable(role = Role.Button, onClick = onClick)
+    )
+}
