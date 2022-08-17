c8cb43cd8cf8f68068f3cb3f1c7c8695e4e5d25b
UlopLT <uloplt@gmail.com>
2022-08-17 23:43:13 +0300

    Add sample users data with avatars

--- src/main/kotlin/data/User.kt
+++ src/main/kotlin/data/User.kt
@@ -3,3 +3,13 @@ package data
 data class User(val name: String, val sureName: String, val avatar: String, val level: Int)
 
 fun getSampleUserData(): User = User("Ramona", "Frederica", "avatars/female.png", 12)
+
+fun getSampleUsersData(): List<User> = buildList {
+    add(User("Wilma", "Wilson", "avatars/wilma_wilson.jpg", 12))
+    add(User("Arlene", "Schmidt", "avatars/arlene_schmidt.jpg", 12))
+    add(User("Andre", "Vasquez", "avatars/andrea_vasquez.jpg", 12))
+    add(User("Constance", "Snyder", "avatars/constance_snyder.jpg", 12))
+    add(User("Marjorie", "Pena", "avatars/marjorie_pena.jpg", 12))
+    add(User("Alyssa", "Dixon", "avatars/alyssa_dixon.jpg", 12))
+    add(User("Jimmie", "Welch", "avatars/ji", 12))
+}
